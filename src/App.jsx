2cde12d2e41b736f1f863285db881d16aec940e2
Nata2312J <ntaliagarcia.c1220@gmail.com>
2023-11-12 17:58:30 -0500

    Se agrega html de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,46 @@ function App() {
 
   return (
     <>
+    <header className='headerViveTerpel'>
+      <div>
+      <img src="src\logoViveTerpel.png" alt="LogoVIveTerpel" className='logoViveTerpel' />
+      </div>
+      <a href="">Inicio</a>
+      <a href="">¿Cómo funciona?</a>
+      <a href="">Mapas</a>
+      <a href="">Aliados</a>
+      <a href="">Centro de ayuda</a>
+    </header> 
+     
+     <main className='mainViveTerpel'>
+      <button>Conocenos</button>
+      <h1>¿Qué es ViveTerpel?</h1>
+      <p>Descubre nuestro nuevo programa de beneficios en el que acomulas puntos facil y rapido y los vives de verdad, disfrutrando como quieres.</p>
+      <button className='edsParticipantesBoton'>Conoce EDS participantes</button>
+     </main>
+
+     <article className='articleViveTerpel'>
+      <section>
+        <img src="src\registrate.png" alt="" />
+        <h3>Registrate y gana puntos</h3>
+        <p>Por ser parte de VIve Terpel recibe puntos de bienvenida por tu primera compra.</p>
+      </section> 
+      <section>
+      <img src="src\puntosCompra.png" alt="" />
+        <h3>Puntos por tus compras</h3>
+        <p>Recuerda dar tu cedula al comprar en una EDS participante o cualquier tienda Altoque para acumular puntos.</p>
+      </section>
+      <section>
+      <img src="src\estacionesCercanas.png" alt="" />
+        <h3>Estaciones más cercanas</h3>
+        <p>Te mostramos nuestra red de Estaciones de Servicio para que verifiques los puntos más cerca a ti</p>
+      </section>
+      <section>
+      <img src="src\Beneficios.png" alt="" />
+        <h3>Beneficios exclusivos para ti</h3>
+        <p>Por ser parte de ViveTerpel disfrutas de promociones únicas, descuentos increíbles y beneficios exclusivos.</p>
+      </section>
+     </article> 
         <Registro/>
     </>
   ); 
